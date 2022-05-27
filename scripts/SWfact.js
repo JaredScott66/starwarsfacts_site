cc5ade20f5ac272f3f331fcda9a7565552f6990e
jaredscott66 <jaredstscott@juno.com>
2022-05-26 22:36:50 -0500

    first commit

---
+++ scripts/SWfact.js
@@ -0,0 +1,20 @@
+const url = "https://run.mocky.io/v3/e871d73b-0f1f-4651-9777-d0d78aaa2f5e";
+
+let result = null;
+async function factGrap(url) {
+    const response = await fetch(url);
+    if (response.ok) {
+        const fact = await response.json();
+        addSite(fact);
+    }
+}
+
+function addSite(data) {
+    result = data;
+    const outputElement = document.querySelector("#fact");
+    const html = `<h2>${result.name}</h2>`;
+    outputElement.innerHTML = html;
+    console.log("fact: ", result);
+}
+factGrap(url);
+console.log("fact: ", result)
